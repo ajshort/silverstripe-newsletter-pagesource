27c4ce6b9a5b608836c54f4d1cc933715ab7714a
ajshort <andrewjshort@gmail.com>
2011-10-09 02:54:46 +1100

    Imported the emogrifier library.

--- _config.php
+++ _config.php
@@ -3,5 +3,9 @@
  * @package silverstripe-newsletter-pagesource
  */
 
+set_include_path(
+	BASE_PATH . '/newsletter-pagesource/thirdparty' . PATH_SEPARATOR . get_include_path()
+);
+
 Object::add_extension('Newsletter', 'NewsletterPageSourceExtension');
 Object::add_extension('NewsletterEmail', 'NewsletterEmailPageSourceExtension');
